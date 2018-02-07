417d3ab81bd41554e46fe8c8e278ea29930121f9
Andrey Germanov <andrey@it-port.ru>
2018-02-07 18:56:21 +0300

    Initial commit

---
+++ src/test/java/com/coinportal/CoinPortalApplicationTests.java
@@ -0,0 +1,16 @@
+package com.coinportal;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CoinPortalApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
